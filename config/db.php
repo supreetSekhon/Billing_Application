ad3acc326e8928395bf6e8473f362e47cfc675d1
Supreet Sekhon <sekhonsupreet@yahoo.in>
2024-06-10 15:16:47 -0400

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=dzc_cms',
+    'dsn' => 'mysql:host=localhost;dbname=cms',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
